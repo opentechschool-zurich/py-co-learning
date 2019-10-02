a2fcfc86518170c1cae3927caa82926e596bdbbf
ale rimoldi <ale@graphicslab.org>
2019-10-02 08:48:03 +0200

    ingore lowere/upper cases and accept sentences

--- 20-min-projects/hangman/ale/hangman.py
+++ 20-min-projects/hangman/ale/hangman.py
@@ -1,16 +1,16 @@
 word = input('the word : ')
-found = ''
+found = ' '
 lifes = 4
 
 while lifes > 0:
-    output = ''.join([letter if letter in found else '-'
+    output = ''.join([letter if letter.lower() in found else '-'
                       for letter in word])
     print(output)
     if output == word:
         print('you won')
         break
-    guess = input('your guess: ')
-    if guess in word:
+    guess = input('your guess: ').lower()
+    if guess in word.lower():
         found += guess
     else:
         lifes -= 1
