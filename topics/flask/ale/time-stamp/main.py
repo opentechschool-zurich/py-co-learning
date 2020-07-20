e0992960b2ce6260e2c35eaaa1fcda801c6e0e1e
ale rimoldi <ale@graphicslab.org>
2020-07-20 09:37:06 +0200

    add the jinja template and the requirements

--- topics/flask/ale/time-stamp/main.py
+++ topics/flask/ale/time-stamp/main.py
@@ -1,8 +1,24 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
 app.config['DEBUG'] = True
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db', 'dbTest.db')
+db = SQLAlchemy(app)
+
+class Log_entries(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String(80), nullable=True)
+    timestamp = db.Column(db.DateTime(timezone=True), default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Timestamp %r>' % self.timestamp
 
 @app.route('/', methods=['GET'])
 def get_index():
